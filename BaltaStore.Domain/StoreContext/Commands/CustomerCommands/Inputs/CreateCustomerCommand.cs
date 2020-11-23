798aeadc6507fccef492ef27beb252e759784dce
Victor <victorcuru@gmail.com>
2020-11-23 14:47:37 -0300

    Aplicando validaçoes nos commands criados

--- BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -1,10 +1,30 @@
+using BaltaStore.Shared.Commands;
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable, ICommand
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        // Fail Fast Validation
+        public bool IsValid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasMinLen(FirstName, 3, "FirstName", "Deve conter no mínimo 3 caracteres")
+                .HasMaxLen(FirstName, 30, "FirstName", "Deve conter no máximo 30 caracteres")
+                .HasMinLen(LastName, 3, "LastName", "Deve conter no mínimo 3 caracteres")
+                .HasMaxLen(LastName, 30, "LastName", "Deve conter no máximo 30 caracteres")
+                .IsEmail(Email, "Email", "Este e-mail não é válido!")
+                .HasLen(Document, 11, "Document", "CPF inválido!")
+            );
+
+            return IsValid();
+        }
     }
 }
--- BaltaStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
+++ BaltaStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -1,12 +1,28 @@
 using System;
 using System.Collections.Generic;
+using BaltaStore.Shared.Commands;
+using FluentValidator;
+using FluentValidator.Validation;
 
 namespace BaltaStore.Domain.StoreContext.Commands.OrderCommands.Inputs
 {
-    public class PlaceOrderCommand
+    public class PlaceOrderCommand : Notifiable, ICommand
     {
+        public PlaceOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
         public Guid Customer { get; set; }
         public IList<OrderItemCommand> OrderItems { get; set; }
+
+        public bool IsValid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasLen(Customer.ToString(), 36, "Usuário", "Identificador do cliente inválido")
+                .IsGreaterThan(OrderItems.Count, 0, "Items", "Nenhum item foi encontrado")
+            );
+            return IsValid();
+        }
     }
 
     public class OrderItemCommand
