bc0dcc2d5bd2d7ea6842f587ee2e8ee8751166a8
Victor <victorcuru@gmail.com>
2020-11-27 22:39:04 -0300

    Adicionando Mvc no startup

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -16,6 +16,7 @@ namespace BaltaStore.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,10 @@ namespace BaltaStore.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
