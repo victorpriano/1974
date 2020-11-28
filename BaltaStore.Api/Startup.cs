74b8358b5334351647f057116fb12dc1c79289c4
Victor <victorcuru@gmail.com>
2020-11-27 22:58:15 -0300

    adicionando controller e ajustando mvc

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +12,8 @@ namespace BaltaStore.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +24,9 @@ namespace BaltaStore.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
-
             app.UseRouting();
+
+            app.UseMvc();
 
         }
     }
